283915398586618a18b4eb7bd61dac6a38660496
Denis Bakharev <denis.bahareff@yandex.ru>
2019-12-24 01:13:58 +0300

    Refactoring after code review

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import './styles/style.css';
-import {Api} from './scripts/Api';
-import {CardList} from './scripts/CardList';
-import {Card} from './scripts/Card';
-import {Popup} from './scripts/Popup';
+import {
+    Api
+} from './scripts/Api';
+import {
+    CardList
+} from './scripts/CardList';
+import {
+    Card
+} from './scripts/Card';
+import {
+    Popup
+} from './scripts/Popup';
 
 // переменные
 
 export const serverUrl = NODE_ENV === 'development' ?
-'http://praktikum.tk/cohort4' : 'https://praktikum.tk/cohort4';
+    'http://praktikum.tk/cohort4' : 'https://praktikum.tk/cohort4';
 export const root = document.querySelector('.root');
 export const placesList = root.querySelector('.places-list');
 export const popupNew = root.querySelector('.popup');
@@ -34,31 +42,31 @@ export const tokenApi = 'e3c11277-8568-44d8-8899-1627e817d3a6';
 
 export const errorCollection = { // коллекция ошибок валидации
 
-  errorAlways: 'Это обязательное поле',
-  errorLength: 'Должно быть от 2 до 30 символов',
-  errorLink: 'Здесь должна быть ссылка',
-  load: 'Загрузка...',
-  save: 'Сохранить',
-  plus: '+',
-  remove: 'Вы действительно хотите удалить эту карточку?',
-  ESCAPE_CODE: 27
+    errorAlways: 'Это обязательное поле',
+    errorLength: 'Должно быть от 2 до 30 символов',
+    errorLink: 'Здесь должна быть ссылка',
+    load: 'Загрузка...',
+    save: 'Сохранить',
+    plus: '+',
+    remove: 'Вы действительно хотите удалить эту карточку?',
+    ESCAPE_CODE: 27
 };
 
-const loadInfo = new Api(serverUrl + 'users/me', tokenApi).  // загрузка инфо о пользователе с сервера
+const loadInfo = new Api(serverUrl + 'users/me', tokenApi). // загрузка инфо о пользователе с сервера
 getInitialInfo()
-.then(info => {
-  
-  const userInfoName = document.querySelector('.user-info__name');
-  const userInfoJob = document.querySelector('.user-info__job');
-    
-  document.querySelector('.user-info__photo').
-  setAttribute('style', `background-image: url(${info.avatar})`);
-  userInfoName.textContent = info.name;
-  userInfoJob.textContent = info.about;
-  userNameInput.value = info.name;
-  userAboutInput.value = info.about;
-  submitActiveFormEdit();
-}) 
+    .then(info => {
+
+        const userInfoName = document.querySelector('.user-info__name');
+        const userInfoJob = document.querySelector('.user-info__job');
+
+        document.querySelector('.user-info__photo').
+        setAttribute('style', `background-image: url(${info.avatar})`);
+        userInfoName.textContent = info.name;
+        userInfoJob.textContent = info.about;
+        userNameInput.value = info.name;
+        userAboutInput.value = info.about;
+        submitActiveFormEdit();
+    })
 
 export const existingCard = new CardList(placesList);
 
@@ -67,16 +75,16 @@ existingCard.render();
 const open = new Popup();
 
 function validButton() { // активация кнопки добавить или сохранить в попапе
-  
-  if (this.checkValidity()) {
 
-    this.querySelector('[type=submit]').
-    setAttribute('style', 'background-color: black; color: white;');
-    this.querySelector('[type=submit]').disabled = !this.checkValidity();
-  } else {
-    
-    this.querySelector('[type=submit]').
-    removeAttribute('style', 'background-color: black; color: white;');
-    this.querySelector('[type=submit]').disabled = !this.checkValidity();
-  }
+    if (this.checkValidity()) {
+
+        this.querySelector('[type=submit]').
+        setAttribute('style', 'background-color: black; color: white;');
+        this.querySelector('[type=submit]').disabled = !this.checkValidity();
+    } else {
+
+        this.querySelector('[type=submit]').
+        removeAttribute('style', 'background-color: black; color: white;');
+        this.querySelector('[type=submit]').disabled = !this.checkValidity();
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,72 +8,77 @@ const isDev = process.env.NODE_ENV === 'development';
 
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+        main: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[chunkhash].js'
-        },
-module: {
-    rules: [{ 
-        test: /\.js$/, 
-        use: { loader: "babel-loader" }, 
-        exclude: /node_modules/ 
-        },
-        {
-        test: /\.(woff|woff2|ttf|otf|png|jpe?g|gif|svg)$/i,
-        use: [
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
             {
-        loader: 'file-loader',
-        options: {
-            name: 'images/[name].[ext]',
-            esModule: false
-        }
-        },
-        {
-        loader: 'image-webpack-loader',
-        options: {
-            bypassOnDebug: true,
-            disable: true,
-        }
-        }
-        ]
-        },
-        {
-        test: /\.css$/, 
-        use: [
-            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-            'css-loader', 
-            'postcss-loader'
-        ]
-        },
+                test: /\.(png|jpg|gif|ico|svg)$/,
+                use: [
+                    'file-loader?name=images/[name].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            esModule: false,
+                            bypassOnDebug: true,
+                            disable: true,
+                        }
+                    },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=./vendor/[name].[ext]'
+            }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
         ]
     },
     resolve: {
         alias: {
             images: path.resolve(__dirname, 'images')
-    }
+        }
     },
     devServer: {
         overlay: true
     },
-plugins: [ 
-    new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
-    new OptimizeCssAssetsPlugin({
-        assetNameRegExp: /\.css$/g,
-        cssProcessor: require('cssnano'),
-        cssProcessorPluginOptions: {
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
                 preset: ['default'],
-        },
-        canPrint: true
-    }),
-    new HtmlWebpackPlugin({
-        inject: false,
-        template: './src/index.html',
-        filename: 'index.html'
-    }),
-    new WebpackMd5Hash(),
-    new webpack.DefinePlugin({
-    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-})
-]
+            },
+            canPrint: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new WebpackMd5Hash(),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
 };
