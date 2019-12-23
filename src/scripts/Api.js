cd1722a82f4984ea057c0158070d7bb5d3b19da6
Denis Bakharev <denis.bahareff@yandex.ru>
2019-12-24 01:13:58 +0300

    Refactoring after code review

--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -1,154 +1,155 @@
 export class Api {
   constructor(url, token) {
-    this.url = url;
-    this.token = token;
+      this.url = url;
+      this.token = token;
   }
 
   getInitialInfo() {
-    return fetch(this.url, {
-      headers: {
-        authorization: this.token,
-      },
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      return fetch(this.url, {
+              headers: {
+                  authorization: this.token,
+              },
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   getInitialCards() {
-    return fetch(this.url, {
-      headers: {
-        authorization: this.token,
-      },
-    })
-      .then((resj) => {
-        this.responseСheck(resj);
-        return resj.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      return fetch(this.url, {
+              headers: {
+                  authorization: this.token,
+              },
+          })
+          .then((resj) => {
+              this.responseСheck(resj);
+              return resj.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   postInfo(json) {
-    this.json = json;
-    return fetch(this.url, {
+      this.json = json;
+      return fetch(this.url, {
 
-      method: 'PATCH',
-      headers: {
-        authorization: this.token,
-        'Content-Type': 'application/json',
-      },
-      body: this.json,
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'PATCH',
+              headers: {
+                  authorization: this.token,
+                  'Content-Type': 'application/json',
+              },
+              body: this.json,
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   postCard(json) {
-    this.json = json;
-    return fetch(this.url, {
+      this.json = json;
+      return fetch(this.url, {
 
-      method: 'POST',
-      headers: {
-        authorization: this.token,
-        'Content-Type': 'application/json',
-      },
-      body: this.json,
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'POST',
+              headers: {
+                  authorization: this.token,
+                  'Content-Type': 'application/json',
+              },
+              body: this.json,
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   reCard() {
-    return fetch(this.url, {
+      return fetch(this.url, {
 
-      method: 'DELETE',
-      headers: {
-        authorization: this.token,
-      },
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'DELETE',
+              headers: {
+                  authorization: this.token,
+              },
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   likeCard() {
-    return fetch(this.url, {
+      return fetch(this.url, {
 
-      method: 'PUT',
-      body: this.json,
-      headers: {
-        authorization: this.token,
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'PUT',
+              body: this.json,
+              headers: {
+                  authorization: this.token,
+                  'Content-Type': 'application/json',
+              },
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   deleteLikeCard() {
-    return fetch(this.url, {
+      return fetch(this.url, {
 
-      method: 'DELETE',
-      headers: {
-        authorization: this.token,
-      },
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'DELETE',
+              headers: {
+                  authorization: this.token,
+              },
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
   addAvatar(json) {
-    this.json = json;
-    return fetch(this.url, {
+      this.json = json;
+      return fetch(this.url, {
 
-      method: 'PATCH',
-      headers: {
-        authorization: this.token,
-      },
-      body: this.json,
-    })
-      .then((res) => {
-        this.responseСheck(res);
-        return res.json();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+              method: 'PATCH',
+              headers: {
+                  authorization: this.token,
+              },
+              body: this.json,
+          })
+          .then((res) => {
+              this.responseСheck(res);
+              return res.json();
+          })
+          .catch((err) => {
+              console.log(err);
+          });
   }
 
-  responseСheck(res) {
-    if (res.ok) {
-      return res;
-    } return Promise.reject(res.status);
+  responseCheck(res) {
+      if (res.ok) {
+          return res;
+      }
+      return Promise.reject(res.status);
   }
 }
